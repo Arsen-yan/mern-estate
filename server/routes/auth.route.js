c9b9579bce726f1153303fe1516baaf12184a28c
Arsen-yan <arsensencho92@gmail.com>
2024-08-08 14:55:05 +0200

    complete sign up page functionality

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup } from "../controllrts/auth.controller.js";
+import { signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
