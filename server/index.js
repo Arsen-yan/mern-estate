90ed16e2e7f4504801a14e6dfaf777b63f01cbec
Arsen-yan <arsensencho92@gmail.com>
2024-08-12 15:49:00 +0200

    create update user api route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -17,13 +18,15 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
   res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
   next();
 });
-
-app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
